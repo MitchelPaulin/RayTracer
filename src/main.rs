4a3fb2238177581fc5c909b786bf8034d622c0e4
MitchelPaulin <mitchel0022@gmail.com>
2022-04-10 19:39:59 -0400

    Added sphere drawing to main

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,32 @@
 #![allow(dead_code, non_snake_case)]
 
-use std::f32::consts::PI;
-
 use draw::{canvas::Canvas, color::Color};
-use math::{matrix::Matrix, tuples::Tuple};
+use math::{matrix::Matrix, ray::Ray, tuples::Tuple};
+use shapes::{intersect::Intersect, sphere::Sphere};
 
 mod draw;
 mod math;
 mod shapes;
 fn main() {
-    let mut c = Canvas::new(100, 100);
+    let mut c = Canvas::new(1000, 1000);
 
-    let black = Color::new(0.0, 0.0, 0.0);
+    let sphere_transform = &Matrix::translation(500., 500., 0.) * &Matrix::scaling(100., 100., 1.0);
 
-    let origin = Tuple::point(50., 50., 0.0);
-    let mut hand = Tuple::vector(0.0, 25.0, 0.0);
+    let sphere = Sphere::new(Some(sphere_transform));
 
-    let rotation = Matrix::rotation_z(PI / 6.0);
-
-    for _ in 0..12 {
-        let cur = origin + hand;
-        c.write_pixel(cur.x as usize, cur.y as usize, black);
-        hand = &rotation * &hand;
+    for x in 0..1000 {
+        for y in 0..1000 {
+            let ray = Ray::new(
+                Tuple::point(x as f32, y as f32, 0.0),
+                Tuple::vector(0.0, 0.0, -1.0),
+            );
+            if !sphere.intersect(&ray).is_empty() {
+                c.write_pixel(x, y, Color::new(1.0, 0.0, 0.0));
+            } else {
+                c.write_pixel(x, y, Color::new(0.0, 0.0, 0.0));
+            }
+        }
+        println!("Finished line {} out of 1000", x);
     }
 
     c.write_to_ppm();
